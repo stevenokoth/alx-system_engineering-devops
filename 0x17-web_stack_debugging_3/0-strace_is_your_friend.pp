edf6a55f59b51b6e3a0fddbe732e0d7b72d6c1dc
stevenokoth <stevenocholao@gmail.com>
2023-08-09 22:25:59 +0300

    Webstuck

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,7 @@
-# Using strace, find out why Apache
-# is returning a 500 error.
-
-
-exec {'Fixer':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/bin/:/usr/local/bin/:/bin/'
+#!/usr/bin/env puppet
+# fix internal server error
+exec { 'web stack deubging':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path     => '/usr/local/bin/:/bin/',
+  provider => 'shell'
 }
